03c5ee7abb7d6d5312f2598e5ad12602450a8758
Daniel Palstra <daniel@danielpalstra.com>
2021-02-01 21:39:37 +0100

    Bump version: 0.1.3 → 0.1.4

--- statping/__init__.py
+++ statping/__init__.py
@@ -3,4 +3,4 @@ from .statping import Statping
 """Top-level package for pystatping."""
 
 __author__ = """Daniel Palstra"""
-__version__ = "__version__ = '0.1.3'"
+__version__ = "__version__ = '0.1.4'"
