58f5a94c1e8a0d9216389d424612b95b73ba1005
Daniel Palstra <daniel@danielpalstra.com>
2021-02-01 21:01:53 +0100

    Publish on tags

--- statping/__init__.py
+++ statping/__init__.py
@@ -3,5 +3,4 @@ from .statping import Statping
 """Top-level package for pystatping."""
 
 __author__ = """Daniel Palstra"""
-__email__ = "daniel@danielpalstra.com"
 __version__ = "__version__ = '0.1.1'"
